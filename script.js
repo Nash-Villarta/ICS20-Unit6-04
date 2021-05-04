d03908b3f01cb1f2361809b9a1a9f239e598d6bc
Nash Villarta <>
2021-05-04 16:56:16 +0000

    CSS pass

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const half = 0.5
 let area = 0
 
 document.getElementById('button').addEventListener('click', calculate)
-//  Looking for the numbers within the input once clicked
 function calculate () {
   base = document.getElementById('base').value
   base = parseInt(base)
@@ -13,7 +12,6 @@ function calculate () {
   height = parseInt(height)
   basetwo = document.getElementById('basetwo').value
   basetwo = parseInt(basetwo)
-  //  Like with math, starting off with addition of the two base values, then divide them by two, and then following by multiplying them by the height to get the area of a trapezoid.
   area = base + basetwo
   area = area * half
   area = area * height
